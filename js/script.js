40da30a8a097d93993386730c2ca44297e75d231
MisterValiant <81043907+MisterValiant@users.noreply.github.com>
2022-09-17 17:50:00 +0400

    Section one optimisation
    +Compressed hero img
    +Started section 2
    +SEO for anchor href

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-var fireAudio = new Audio('audio/missile-launch.wav');
-var targetStatus = document.querySelector(".target-status");
-var targetDestroyed = 0;
-var targetMissed = 0;
+let fireAudio = new Audio('audio/missile-launch.wav');
+const targetStatus = document.querySelector(".target-status");
+let targetDestroyed = 0;
+let targetMissed = 0;
 
-var nudgeAnimation = document.querySelector('.crosshair');
+const nudgeAnimation = document.querySelector('.crosshair');
 nudgeAnimation.classList.remove('nudge');
 
 window.onload = function () {
     document.querySelector('.hero').classList.add('blink3s');
     setTimeout(heroGUI, 2500);
+    setTimeout(dev, 3000);
+}
+
+function dev(){
+    alert('Website still under construction! Come check it out later ;)');
 }
 
 function heroGUI() {
@@ -33,7 +38,7 @@ function fire() {
             targetDestroyed = parseInt(targetDestroyed);
             targetMissed = parseInt(targetMissed);
 
-            var missileLoaded = document.querySelector(".missile-loaded");
+            const missileLoaded = document.querySelector(".missile-loaded");
             missileLoaded.textContent = 0;
             missileLoaded.classList.add('blink');
 
@@ -47,7 +52,7 @@ function fire() {
             }
 
         } else {
-            var denyAudio = new Audio('audio/deny-launch.wav');
+            const denyAudio = new Audio('audio/deny-launch.wav');
             denyAudio.volume = 0.5;
             denyAudio.currentTime = 0;
             denyAudio.play();
@@ -60,13 +65,13 @@ function explosion() {
     targetStatus.classList.add("blink");
     targetStatus.classList.add("destroyed");
 
-    var boom = document.querySelector('.neutralised');
+    const boom = document.querySelector('.neutralised');
     targetDestroyed++;
     boom.classList.add('blink');
     boom.textContent = targetDestroyed;
 
     targetStatus.textContent = 'Target Down --✴';
-    var destroyAudio = new Audio('audio/explosion.wav');
+    const destroyAudio = new Audio('audio/explosion.wav');
     fireAudio.volume = 0.09;
     destroyAudio.volume = 0.18;
     destroyAudio.currentTime = 0;
@@ -74,7 +79,7 @@ function explosion() {
 }
 
 function destroy() {
-    var destroyAudio = new Audio('audio/neutralised.mp3');
+    const destroyAudio = new Audio('audio/neutralised.mp3');
     destroyAudio.volume = 0.3;
     destroyAudio.currentTime = 0;
     destroyAudio.play();
@@ -96,7 +101,7 @@ function miss() {
     targetStatus.classList.add("missed");
     targetStatus.textContent = 'Target Missed --🛦';
 
-    var boom = document.querySelector('.missile-missed');
+    const boom = document.querySelector('.missile-missed');
     targetMissed++;
     boom.classList.add('blink');
     boom.textContent = targetMissed;
@@ -126,7 +131,7 @@ function resetHUD() {
     targetStatus.classList.remove("missed");
     targetStatus.textContent = 'Engage Target --🛦';
 
-    var missileLoaded = document.querySelector(".missile-loaded")
+    const missileLoaded = document.querySelector(".missile-loaded")
     missileLoaded.textContent = 1;
     missileLoaded.classList.remove('blink');
     document.querySelector('.neutralised').classList.remove('blink');
